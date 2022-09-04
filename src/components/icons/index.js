aa9eafb5d2eee1387f6eb6386771db3f788079db
totsawin <i_amnothere@live.com>
2022-09-04 11:16:53 +0700

    first commit - skeleton

---
+++ src/components/icons/index.js
@@ -0,0 +1,15 @@
+export { default as IconAppStore } from './appstore.astro';
+export { default as IconBookmark } from './bookmark.astro';
+export { default as IconCodepen } from './codepen.astro';
+export { default as IconExternal } from './external.astro';
+export { default as IconFolder } from './folder.astro';
+export { default as IconFork } from './fork.astro';
+export { default as Icon } from './icon.astro';
+export { default as IconGitHub } from './github.astro';
+export { default as IconInstagram } from './instagram.astro';
+export { default as IconLinkedin } from './linkedin.astro';
+export { default as IconLoader } from './loader.astro';
+export { default as IconLogo } from './logo.astro';
+export { default as IconPlayStore } from './playstore.astro';
+export { default as IconStar } from './star.astro';
+export { default as IconTwitter } from './twitter.astro';
