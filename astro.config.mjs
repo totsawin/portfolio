0cb6948f9bfad23691ae752cfbbe38a9c2e7986b
totsawin <i_amnothere@live.com>
2022-09-22 14:50:19 +0700

    replace img and picture with Image and Picture Astro component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
 import svelte from "@astrojs/svelte";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), svelte()]
+  integrations: [react(), svelte(), image()]
 });
