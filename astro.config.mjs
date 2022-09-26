65949e30cc6f1088e5a929e1ab44ad3376671d28
totsawin <i_amnothere@live.com>
2022-09-26 13:02:06 +0700

    switch to deploy with vercel instead of netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import svelte from "@astrojs/svelte";
 import image from "@astrojs/image";
-
 import netlify from "@astrojs/netlify/functions";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte(), image()],
   output: "server",
-  adapter: netlify()
+  adapter: vercel()
 });
