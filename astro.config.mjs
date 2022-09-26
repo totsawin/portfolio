d62d38016f61f114892ef53a746dc4034d9531bd
totsawin <i_amnothere@live.com>
2022-09-26 15:47:59 +0700

    swtch the deploy option back to netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,12 @@ import { defineConfig } from 'astro/config';
 import svelte from "@astrojs/svelte";
 import image from "@astrojs/image";
 import netlify from "@astrojs/netlify/functions";
-
 import vercel from "@astrojs/vercel/serverless";
+
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte(), image()],
   output: "server",
-  adapter: vercel()
+  adapter: netlify()
 });
