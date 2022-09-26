82456acbf9875cf004745e2ed0267d89221f8409
totsawin <i_amnothere@live.com>
2022-09-26 10:57:44 +0700

    remove unused dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config';
-import react from "@astrojs/react";
 import svelte from "@astrojs/svelte";
 
 import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), svelte(), image()]
+  integrations: [svelte(), image()]
 });
