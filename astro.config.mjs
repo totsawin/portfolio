0c4c2553de09c6e0498dd84efbea467b826289a2
totsawin <i_amnothere@live.com>
2022-09-29 15:19:25 +0700

    revert from Astro Image component to common img and picture tags because of issue on deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
 import { defineConfig } from 'astro/config';
 import svelte from "@astrojs/svelte";
-import image from "@astrojs/image";
 import netlify from "@astrojs/netlify/functions";
-import vercel from "@astrojs/vercel/serverless";
-
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), image()],
+  integrations: [svelte()],
   output: "server",
   adapter: netlify()
 });
