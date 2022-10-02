a8f0baba5e74dd127f32cde5b63bc40e5fb0d6c0
totsawin <i_amnothere@live.com>
2022-10-03 03:04:15 +0700

    rollback to deploy with netlify functions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify/edge-functions';
+import netlify from '@astrojs/netlify/functions';
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
