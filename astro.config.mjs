03cb9ea3331ec6b5a4163225a2a29bb27b21f15e
totsawin <i_amnothere@live.com>
2022-10-03 02:49:26 +0700

    try netlify edge fucntion

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
+
 import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify/edge-functions';
 import svelte from "@astrojs/svelte";
-import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
